625c6d6857d9d0a770e002d0db1ae09a0e1d29eb
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-04-07 10:34:04 +0200

    refactor: move models to own file

--- pkg/azure/client.go
+++ pkg/azure/client.go
@@ -12,39 +12,6 @@ import (
 	"github.com/nais/azureator/pkg/apis/v1alpha1"
 )
 
-type Client interface {
-	RegisterOrUpdateApplication(credential v1alpha1.AzureAdCredential) (Credentials, error)
-	DeleteApplication(credential v1alpha1.AzureAdCredential) error
-}
-
-type client struct {
-	ctx                    context.Context
-	config                 *Config
-	servicePrincipalClient graphrbac.ServicePrincipalsClient
-	applicationsClient     graphrbac.ApplicationsClient
-}
-
-type Credentials struct {
-	Public  Public  `json:"public"`
-	Private Private `json:"private"`
-}
-
-type Public struct {
-	ClientId string `json:"clientId"`
-	Key      Key    `json:"key"`
-}
-
-type Private struct {
-	ClientId     string `json:"clientId"`
-	ClientSecret string `json:"clientSecret"`
-	Key          Key    `json:"key"`
-}
-
-type Key struct {
-	KeyBase64 string `json:"keyBase64"`
-	KeyId     string `json:"keyId"`
-}
-
 const (
 	IntegratedAppTag string = "WindowsAzureActiveDirectoryIntegratedApp"
 	SignInAudience   string = "AzureADMyOrg"
