5c18cecd0745546b0c6a289e1f0a26c8c3dfc1f3
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-04-28 13:14:07 +0200

    feat: add preauthorized applications on create

--- pkg/azure/client/create.go
+++ pkg/azure/client/create.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/nais/azureator/pkg/apis/v1alpha1"
 	"github.com/nais/azureator/pkg/azure"
 	"github.com/nais/azureator/pkg/azure/util"
 	"github.com/nais/azureator/pkg/util/crypto"
 	"github.com/yaegashi/msgraph.go/ptr"
 	msgraph "github.com/yaegashi/msgraph.go/v1.0"
+)
+
+const (
+	OAuth2DefaultAccessScope string = "defaultaccess"
 )
 
 // Create registers a new AAD application
@@ -35,6 +40,8 @@ func (c client) registerApplication(ctx context.Context, credential v1alpha1.Azu
 		return azure.Application{}, fmt.Errorf("failed to update password credentials for application: %w", err)
 	}
 
+	// TODO - add identifierUri to application
+
 	return azure.Application{
 		Credentials: azure.Credentials{
 			Public: azure.Public{
@@ -55,6 +62,10 @@ func (c client) registerApplication(ctx context.Context, credential v1alpha1.Azu
 }
 
 func createApplication(credential v1alpha1.AzureAdCredential, keyCredential msgraph.KeyCredential) *msgraph.Application {
+	oauthDefaultAccessScopeId := uuid.New().String()
+	msgraphOauthDefaultAccessScopeId := msgraph.UUID(oauthDefaultAccessScopeId)
+	preAuthorizedApplications := mapToPreAuthorizedApplications(credential, oauthDefaultAccessScopeId)
+
 	return &msgraph.Application{
 		DisplayName:           ptr.String(credential.GetUniqueName()),
 		GroupMembershipClaims: ptr.String("SecurityGroup"),
@@ -75,7 +86,38 @@ func createApplication(credential v1alpha1.AzureAdCredential, keyCredential msgr
 		RequiredResourceAccess: []msgraph.RequiredResourceAccess{
 			microsoftGraphApiPermissions(),
 		},
+		API: &msgraph.APIApplication{
+			AcceptMappedClaims:          ptr.Bool(true),
+			RequestedAccessTokenVersion: ptr.Int(2),
+			Oauth2PermissionScopes: []msgraph.PermissionScope{
+				{
+					AdminConsentDescription: ptr.String(fmt.Sprintf("Gives adminconsent for scope %s", OAuth2DefaultAccessScope)),
+					AdminConsentDisplayName: ptr.String(fmt.Sprintf("Adminconsent for scope %s", OAuth2DefaultAccessScope)),
+					ID:                      &msgraphOauthDefaultAccessScopeId,
+					IsEnabled:               ptr.Bool(true),
+					Type:                    ptr.String("User"),
+					Value:                   ptr.String(OAuth2DefaultAccessScope),
+				},
+			},
+			PreAuthorizedApplications: preAuthorizedApplications,
+		},
 	}
+}
+
+func mapToPreAuthorizedApplications(credential v1alpha1.AzureAdCredential, defaultAccessPermissionId string) []msgraph.PreAuthorizedApplication {
+	var preAuthorizedApplications []msgraph.PreAuthorizedApplication
+	for _, app := range credential.Spec.PreAuthorizedApplications {
+		// TODO - lookup if ClientId is not present
+		if len(app.ClientId) > 0 {
+			preAuthorizedApplications = append(preAuthorizedApplications, msgraph.PreAuthorizedApplication{
+				AppID: &app.ClientId,
+				DelegatedPermissionIDs: []string{
+					defaultAccessPermissionId,
+				},
+			})
+		}
+	}
+	return preAuthorizedApplications
 }
 
 func microsoftGraphApiPermissions() msgraph.RequiredResourceAccess {
