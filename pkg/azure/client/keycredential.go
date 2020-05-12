a229a49eceffa9678651633126a04c32c450cf8a
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-05-12 20:39:33 +0200

    fix: should return existing keycredential as it is always registered with an app

--- pkg/azure/client/keycredential.go
+++ pkg/azure/client/keycredential.go
@@ -16,6 +16,9 @@ import (
 // There should always be two active keys available at any given time so that running applications are not interfered with.
 func (c client) rotateKeyCredential(tx azure.Transaction) (msgraph.KeyCredential, crypto.JwkPair, error) {
 	existingKeyCredential, err := c.getExistingKeyCredential(tx)
+	if err != nil {
+		return msgraph.KeyCredential{}, crypto.JwkPair{}, err
+	}
 	keyCredential, jwkPair, err := generateNewKeyCredentialFor(tx.Resource)
 	if err != nil {
 		return msgraph.KeyCredential{}, crypto.JwkPair{}, err
@@ -33,12 +36,19 @@ func (c client) getExistingKeyCredential(tx azure.Transaction) (msgraph.KeyCrede
 	if err != nil {
 		return msgraph.KeyCredential{}, err
 	}
+	newestCredential := application.KeyCredentials[0]
 	for _, keyCredential := range application.KeyCredentials {
+		if keyCredential.StartDateTime.After(*newestCredential.StartDateTime) {
+			newestCredential = keyCredential
+		}
 		if string(*keyCredential.KeyID) == tx.Resource.Status.CertificateKeyId {
 			return keyCredential, nil
 		}
 	}
-	return msgraph.KeyCredential{}, fmt.Errorf("failed to find application key matching the previous key ID in Status field")
+	if len(application.KeyCredentials) > 0 {
+		return newestCredential, nil
+	}
+	return msgraph.KeyCredential{}, fmt.Errorf("failed to find key credential for azure application")
 }
 
 func generateNewKeyCredentialFor(resource v1alpha1.AzureAdApplication) (msgraph.KeyCredential, crypto.JwkPair, error) {
