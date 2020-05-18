60016be801f6452eb25f9a5a92a6a1f5076f9d2f
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-05-18 14:46:12 +0200

    test: basic test for azure oauth2permissionscope

---
+++ pkg/azure/client/oauth2permissionscope_test.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOAuth2PermissionScope_defaultScopes(t *testing.T) {
+	a := client{}.oAuth2PermissionScopes().defaultScopes()
+	j, _ := json.Marshal(a)
+
+	assert.JSONEq(t, `
+[
+  {
+    "adminConsentDescription": "Gives adminconsent for scope defaultaccess",
+    "adminConsentDisplayName": "Adminconsent for scope defaultaccess",
+    "id": "00000000-1337-d34d-b33f-000000000000",
+    "isEnabled": true,
+    "type": "User",
+    "value": "defaultaccess"
+  }
+]
+`, string(j))
+}
