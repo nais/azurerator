5bcf7b5fe922a6b7e8ff6c3b87a7c45a50eabf86
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-06-30 07:52:53 +0200

    fix: skip deletion of unused secret if referenced in spec

--- controllers/azureadapplication/azureadapplication_controller.go
+++ controllers/azureadapplication/azureadapplication_controller.go
@@ -145,13 +145,16 @@ func (r *Reconciler) process(tx transaction) error {
 	if err != nil {
 		return fmt.Errorf("failed to create or update secret: %w", err)
 	}
-	logger.Infof("secret %s", res)
+	logger.Infof("secret '%s' %s", tx.instance.Spec.SecretName, res)
 
 	if err := r.updateStatus(tx, application); err != nil {
 		return err
 	}
 
 	for _, oldSecret := range managedSecrets.Unused.Items {
+		if oldSecret.Name == tx.instance.Spec.SecretName {
+			continue
+		}
 		logger.Infof("deleting unused secret '%s'...", oldSecret.Name)
 		if err := secrets.Delete(tx.ctx, oldSecret, r.Client); err != nil {
 			return err
