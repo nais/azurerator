cc773a702c05016aa859419b901e2a0a1981b54d
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-02-12 15:02:01 +0100

    revert: check for empty namespace and cluster for preauthorizedapps

--- controllers/azureadapplication/azureadapplication_controller.go
+++ controllers/azureadapplication/azureadapplication_controller.go
@@ -138,16 +138,6 @@ func (r *Reconciler) prepare(req ctrl.Request) (*transaction, error) {
 	instance.SetClusterName(r.Config.ClusterName)
 	instance.Status.CorrelationId = correlationId
 
-	for _, preAuthApp := range instance.Spec.PreAuthorizedApplications {
-		if len(preAuthApp.Namespace) == 0 {
-			preAuthApp.Namespace = req.Namespace
-		}
-
-		if len(preAuthApp.Cluster) == 0 {
-			preAuthApp.Cluster = r.Config.ClusterName
-		}
-	}
-
 	logger.Info("processing AzureAdApplication...")
 
 	return &transaction{ctx, instance, logger}, nil
