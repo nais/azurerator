1d44c70faa5f84433c277a53c5b13b7a6fe1042c
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2021-11-19 15:28:31 +0100

    fix: do not clean up unused credentials if current set is invalid

--- controllers/azureadapplication/azureadapplication_controller.go
+++ controllers/azureadapplication/azureadapplication_controller.go
@@ -121,7 +121,7 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		return r.HandleError(*tx, err)
 	}
 
-	if tx.Options.Process.Secret.Cleanup {
+	if tx.Options.Process.Secret.Cleanup && tx.Options.Process.Secret.Valid {
 		err = r.Azure().DeleteUnusedCredentials(*tx)
 		if err != nil {
 			return r.HandleError(*tx, err)
